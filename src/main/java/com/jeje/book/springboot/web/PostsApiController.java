fd258ccddd6666455dc956b8bab3810423d37339
hyperminji <hyperminji@gmail.com>
2021-05-28 02:10:27 +0900

    210527

--- src/main/java/com/jeje/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jeje/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.jeje.book.springboot.web;
 
-import com.jeje.book.springboot.posts.PostsService;
+import com.jeje.book.springboot.service.PostsService;
+import com.jeje.book.springboot.web.dto.PostsResponseDto;
 import com.jeje.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jeje.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -18,5 +18,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
 
+    }
+
+    //수정조회기능
+    @PutMapping("/api/v1/posts/{id}")
+    public  Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
